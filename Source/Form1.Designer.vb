00806f22b6d2e72d4b9bc3a9b228f7fd9b270da7
Raphael Luz <luz@ivt.tugraz.at>
2014-09-05 09:34:20 +0200

    Initial Commit

---
+++ Source/Form1.Designer.vb
@@ -0,0 +1,339 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TbToken = New System.Windows.Forms.TextBox()
+        Me.TbDevice = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ChWSlockCtrl = New System.Windows.Forms.CheckBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ChLog = New System.Windows.Forms.CheckBox()
+        Me.BtLogClear = New System.Windows.Forms.Button()
+        Me.ChNotif = New System.Windows.Forms.CheckBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TbUnlock = New System.Windows.Forms.TextBox()
+        Me.TbLock = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GrTest = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.GrTest.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TbToken
+        '
+        Me.TbToken.Location = New System.Drawing.Point(99, 23)
+        Me.TbToken.Name = "TbToken"
+        Me.TbToken.Size = New System.Drawing.Size(298, 20)
+        Me.TbToken.TabIndex = 0
+        '
+        'TbDevice
+        '
+        Me.TbDevice.Location = New System.Drawing.Point(99, 49)
+        Me.TbDevice.Name = "TbDevice"
+        Me.TbDevice.Size = New System.Drawing.Size(298, 20)
+        Me.TbDevice.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(350, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(47, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Push"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Access Token"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 52)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "device_iden"
+        '
+        'ChWSlockCtrl
+        '
+        Me.ChWSlockCtrl.AutoSize = True
+        Me.ChWSlockCtrl.Checked = True
+        Me.ChWSlockCtrl.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.ChWSlockCtrl.Location = New System.Drawing.Point(14, 19)
+        Me.ChWSlockCtrl.Name = "ChWSlockCtrl"
+        Me.ChWSlockCtrl.Size = New System.Drawing.Size(65, 17)
+        Me.ChWSlockCtrl.TabIndex = 7
+        Me.ChWSlockCtrl.Text = "Enabled"
+        Me.ChWSlockCtrl.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(6, 48)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(391, 126)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ChLog)
+        Me.GroupBox1.Controls.Add(Me.BtLogClear)
+        Me.GroupBox1.Controls.Add(Me.RichTextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 257)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(403, 180)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Log"
+        '
+        'ChLog
+        '
+        Me.ChLog.AutoSize = True
+        Me.ChLog.Checked = True
+        Me.ChLog.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.ChLog.Location = New System.Drawing.Point(9, 23)
+        Me.ChLog.Name = "ChLog"
+        Me.ChLog.Size = New System.Drawing.Size(65, 17)
+        Me.ChLog.TabIndex = 0
+        Me.ChLog.Text = "Enabled"
+        Me.ChLog.UseVisualStyleBackColor = True
+        '
+        'BtLogClear
+        '
+        Me.BtLogClear.Location = New System.Drawing.Point(340, 19)
+        Me.BtLogClear.Name = "BtLogClear"
+        Me.BtLogClear.Size = New System.Drawing.Size(57, 23)
+        Me.BtLogClear.TabIndex = 1
+        Me.BtLogClear.Text = "Clear"
+        Me.BtLogClear.UseVisualStyleBackColor = True
+        '
+        'ChNotif
+        '
+        Me.ChNotif.AutoSize = True
+        Me.ChNotif.Checked = True
+        Me.ChNotif.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.ChNotif.Location = New System.Drawing.Point(18, 454)
+        Me.ChNotif.Name = "ChNotif"
+        Me.ChNotif.Size = New System.Drawing.Size(106, 17)
+        Me.ChNotif.TabIndex = 4
+        Me.ChNotif.Text = "Tray notifications"
+        Me.ChNotif.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(93, 26)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(92, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Panel1)
+        Me.GroupBox2.Controls.Add(Me.ChWSlockCtrl)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 101)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(403, 86)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Workstation Lock Commands"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.TbUnlock)
+        Me.Panel1.Controls.Add(Me.TbLock)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Location = New System.Drawing.Point(6, 40)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(393, 40)
+        Me.Panel1.TabIndex = 9
+        '
+        'TbUnlock
+        '
+        Me.TbUnlock.Location = New System.Drawing.Point(301, 10)
+        Me.TbUnlock.Name = "TbUnlock"
+        Me.TbUnlock.Size = New System.Drawing.Size(90, 20)
+        Me.TbUnlock.TabIndex = 1
+        '
+        'TbLock
+        '
+        Me.TbLock.Location = New System.Drawing.Point(87, 10)
+        Me.TbLock.Name = "TbLock"
+        Me.TbLock.Size = New System.Drawing.Size(90, 20)
+        Me.TbLock.TabIndex = 0
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(204, 13)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(91, 13)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Unlock Command"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(0, 13)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Lock Command"
+        '
+        'GrTest
+        '
+        Me.GrTest.Controls.Add(Me.TextBox1)
+        Me.GrTest.Controls.Add(Me.Button1)
+        Me.GrTest.Controls.Add(Me.Label1)
+        Me.GrTest.Location = New System.Drawing.Point(12, 193)
+        Me.GrTest.Name = "GrTest"
+        Me.GrTest.Size = New System.Drawing.Size(403, 58)
+        Me.GrTest.TabIndex = 2
+        Me.GrTest.TabStop = False
+        Me.GrTest.Text = "Test"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(50, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(294, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Note"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label2)
+        Me.GroupBox4.Controls.Add(Me.TbToken)
+        Me.GroupBox4.Controls.Add(Me.TbDevice)
+        Me.GroupBox4.Controls.Add(Me.Label3)
+        Me.GroupBox4.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(403, 83)
+        Me.GroupBox4.TabIndex = 0
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "PushBullet Access"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(427, 486)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GrTest)
+        Me.Controls.Add(Me.ChNotif)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowInTaskbar = False
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GrTest.ResumeLayout(False)
+        Me.GrTest.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TbToken As System.Windows.Forms.TextBox
+    Friend WithEvents TbDevice As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ChWSlockCtrl As System.Windows.Forms.CheckBox
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents BtLogClear As System.Windows.Forms.Button
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ChNotif As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents TbUnlock As System.Windows.Forms.TextBox
+    Friend WithEvents TbLock As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents GrTest As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ChLog As System.Windows.Forms.CheckBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ Source/Form1.vb
@@ -0,0 +1,257 @@
+﻿Imports System.Net
+Imports System.Text
+Imports Microsoft.Win32
+
+Public Class Form1
+
+    Private Const AppVersion As String = "V1.0"
+
+    Private wc As WebClient
+    Private ExitApp As Boolean
+
+    Private FirstStart As Boolean = True
+    Private TrayHelpShown As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+
+        Me.Text = "PushBullet Control " & AppVersion
+        Me.NotifyIcon1.Text = "PushBullet Control " & AppVersion
+        Me.NotifyIcon1.BalloonTipTitle = "PushBullet Control " & AppVersion
+
+        ExitApp = False
+
+        If Not LoadConfig() Then Log("Failed to load config.xml!")
+
+        AddHandler SystemEvents.SessionSwitch, AddressOf WSlockedCheck
+
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As System.EventArgs) Handles Me.Shown
+
+        If Not FirstStart Then
+            Me.Visible = False
+            TrayMsg("PushBullet Control running.")
+            If Me.ChWSlockCtrl.Checked Then
+                TrayMsg("Workstation Lock Commands ENABLED.")
+            Else
+                TrayMsg("Workstation Lock Commands DISABLED!")
+            End If
+        End If
+
+        If Me.ChWSlockCtrl.Checked Then PushNotification(tPushType.WSunlock)
+
+    End Sub
+
+
+    Private Sub Form1_FormClosing(sender As Object, e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+
+        If e.CloseReason = CloseReason.WindowsShutDown Then
+            If Me.ChWSlockCtrl.Checked Then PushNotification(tPushType.WSlock)
+            ExitApp = True
+        End If
+
+        SaveConfig()
+
+        If Not ExitApp Then
+            'Minimizing...
+            e.Cancel = True
+            Me.Visible = False
+
+            If FirstStart And Not TrayHelpShown Then
+                TrayMsg("PushBullet Control is still running. Right-click tray icon to close the application.")
+                TrayHelpShown = True
+            End If
+
+        Else
+            'Closing...
+
+        End If
+
+    End Sub
+
+    Private Sub TestPush()
+        If Me.TextBox1.Text.Trim = "" Then
+            MsgBox("No text entered!")
+        Else
+            PushNotification(tPushType.Test, Me.TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        TestPush()
+    End Sub
+
+    Private Sub PushNotification(ByVal PushType As tPushType, Optional ByVal Cmd As String = "")
+        Dim parameters As String
+        Dim result As String
+        Dim LogTxt As String
+
+        Select Case PushType
+            Case tPushType.Test
+                LogTxt = "Test"
+
+            Case tPushType.WSlock
+                LogTxt = "WS locked"
+                Cmd = Me.TbLock.Text
+
+            Case Else 'tPushType.WSunlock
+                LogTxt = "WS unlocked"
+                Cmd = Me.TbUnlock.Text
+
+        End Select
+
+        If Cmd.Trim = "" Then Exit Sub
+
+        Try
+
+            'New  System.Net.WebClient
+            wc = New WebClient
+
+            'Header with Access Token (Me.TbToken.Text)
+            wc.Headers.Add(HttpRequestHeader.UserAgent, "PushbulletControl" & AppVersion)
+            wc.Headers.Add(HttpRequestHeader.Authorization, String.Format("Basic {0}", Convert.ToBase64String(Encoding.UTF8.GetBytes(Me.TbToken.Text & ":"))))
+            wc.Headers.Add(HttpRequestHeader.ContentType, "application/x-www-form-urlencoded")
+            wc.Headers.Add(HttpRequestHeader.Accept, "application/json")
+
+            'Set target device ID (Me.TbDevice.Text)
+            parameters = "device_iden=" & Me.TbDevice.Text
+
+            'Set Type
+            parameters &= "&type=note"
+
+            'Set message (Cmd)
+            parameters &= "&title=PushbulletControl&body=" & Cmd
+
+            'Push
+            result = wc.UploadString("https://api.pushbullet.com/v2/pushes", parameters)
+
+            Log("[" & LogTxt & "] Push '" & Cmd & "' successful.")
+
+        Catch ex As Exception
+
+            Log("[" & LogTxt & "] Push '" & Cmd & "' failed!")
+
+        End Try
+
+
+
+    End Sub
+
+    Private Sub WSlockedCheck(ByVal sender As Object, ByVal e As SessionSwitchEventArgs)
+
+        If Not Me.ChWSlockCtrl.Checked Then Exit Sub
+
+        If e.Reason = SessionSwitchReason.SessionLock Then
+            PushNotification(tPushType.WSlock)
+        Else
+            PushNotification(tPushType.WSunlock)
+        End If
+    End Sub
+
+    Private Sub Log(ByVal Msg As String)
+
+        If Me.ChLog.Checked Then Me.RichTextBox1.AppendText(Now.ToString & ": " & Msg & vbCrLf)
+
+        If Me.ChNotif.Checked Then TrayMsg(Msg)
+
+    End Sub
+
+    Private Sub TrayMsg(ByVal Msg As String)
+        Me.NotifyIcon1.BalloonTipText = Msg
+        Me.NotifyIcon1.ShowBalloonTip(500)
+    End Sub
+
+    Private Enum tPushType
+        Test
+        WSlock
+        WSunlock
+    End Enum
+
+
+    Private Sub BtLogClear_Click(sender As System.Object, e As System.EventArgs) Handles BtLogClear.Click
+        Me.RichTextBox1.Clear()
+    End Sub
+
+    
+    Private Sub ExitToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        ExitApp = True
+        Me.Close()
+    End Sub
+
+    Private Sub NotifyIcon1_MouseClick(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles NotifyIcon1.MouseClick
+        If e.Button = Windows.Forms.MouseButtons.Left Then Me.Visible = True
+    End Sub
+
+    Private Function LoadConfig() As Boolean
+
+        Dim xml As XDocument
+
+        If IO.File.Exists(My.Application.Info.DirectoryPath & "\config.xml") Then
+            FirstStart = False
+        Else
+            Return True
+        End If
+
+        Try
+            xml = XDocument.Load(My.Application.Info.DirectoryPath & "\config.xml")
+
+            Me.TbToken.Text = xml.<PushBulletControl>.<PushBulletAccess>.<AccessToken>.Value
+            Me.TbDevice.Text = xml.<PushBulletControl>.<PushBulletAccess>.<device_iden>.Value
+
+            Me.ChWSlockCtrl.Checked = xml.<PushBulletControl>.<Config>.<WSlockControl>.Value
+            Me.ChNotif.Checked = xml.<PushBulletControl>.<Config>.<TrayNotification>.Value
+            Me.ChLog.Checked = xml.<PushBulletControl>.<Config>.<LogEnabled>.Value
+
+            Me.TbLock.Text = xml.<PushBulletControl>.<Commands>.<WSlock>.Value
+            Me.TbUnlock.Text = xml.<PushBulletControl>.<Commands>.<WSunlock>.Value
+
+
+        Catch ex As Exception
+            Return False
+        End Try
+
+        Return True
+
+    End Function
+
+    Private Function SaveConfig() As Boolean
+        Dim xml As New XDocument
+        Dim xe0 As XElement
+        Dim xe As XElement
+
+        xe0 = New XElement("PushBulletControl")
+
+        xe = New XElement("PushBulletAccess")
+        xe.Add(New XElement("AccessToken", Me.TbToken.Text))
+        xe.Add(New XElement("device_iden", Me.TbDevice.Text))
+        xe0.Add(xe)
+
+        xe = New XElement("Config")
+        xe.Add(New XElement("WSlockControl", Me.ChWSlockCtrl.Checked))
+        xe.Add(New XElement("TrayNotification", Me.ChNotif.Checked))
+        xe.Add(New XElement("LogEnabled", Me.ChLog.Checked))
+        xe0.Add(xe)
+
+        xe = New XElement("Commands")
+        xe.Add(New XElement("WSlock", Me.TbLock.Text))
+        xe.Add(New XElement("WSunlock", Me.TbUnlock.Text))
+
+        xe0.Add(xe)
+
+        xml.Add(xe0)
+
+        xml.Save(My.Application.Info.DirectoryPath & "\config.xml")
+
+        Return True
+
+    End Function
+
+    Private Sub ChWSlockCtrl_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles ChWSlockCtrl.CheckedChanged
+        Me.Panel1.Enabled = Me.ChWSlockCtrl.Checked
+    End Sub
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles TextBox1.KeyDown
+        If e.KeyCode = Keys.Enter Then TestPush()
+    End Sub
+
+End Class
